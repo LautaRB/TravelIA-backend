0014af35b28bf52ab4bd81fc4f298323003cecaf
LautaRB <lautanba@gmail.com>
2025-06-02 16:02:27 -0300

    Cambios para el panel de admin

--- users/models.py
+++ users/models.py
@@ -5,3 +5,11 @@ class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=15)
     email = models.EmailField(max_length=50)
+    ROLE = [
+            ("ADMIN", "ADMIN"),
+            ("USER", "USER")
+    ]
+    role = models.CharField(choices=ROLE, default=ROLE[1])
+
+    def __str__(self): #Para ver el nombre en la BD
+        return self.username + ' - ' + self.role
