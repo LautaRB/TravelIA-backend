9a902340e86380fbb00e989c0ee9bacc3d4e4c26
Lautaro Rodriguez Brown <lautanba@gmail.com>
2025-06-16 00:01:05 -0300

    Login, Register & Protected_View terminados

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.landing), #Ruta principal
-    path('users/tokens/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # obtiene el par de tokens
     path('users/refreshtoken/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/protected/', views.ProtectedView.as_view(), name='protected'), #Ruta para el usuario autenticado
     path('users/register/', views.register),
--- users/views.py
+++ users/views.py
@@ -2,14 +2,19 @@ from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .serializers import UserSerializer
+from travelia.utils.messeges import MessagesES
 
 # Create your views here.
 class ProtectedView(APIView): # clase para la autenticación
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response({"success": True, "message": "Bienvenido a la API de TravelIA"})
+        return Response({
+            'success': True,
+            'message': f'Hola {request.user.username}, estás autenticado',
+        })
 
 @api_view(['GET'])
 def landing(request):
@@ -22,16 +27,32 @@ def register(request):
 
     if serializer.is_valid():
         serializer.save()
-        return Response({"success": True, "message": "Usuario registrado con éxito"})
+        return Response({"success": True, "message": MessagesES.SUCCESS_REGISTER})
     
     raise ValidationError(serializer.errors)
 
 @api_view(['POST'])
 def login(request):
-    
     username = request.data.get('username')
     password = request.data.get('password')
     
-    print(username, password)
+    if not username:
+        raise ValidationError({"username": "Este campo es requerido" if not username else None})
+    elif not password:
+        raise ValidationError({"password": "Este campo es requerido" if not password else None})
     
-    return Response ('Hello Login!')
+    serializer = TokenObtainPairSerializer(data={
+        'username': username,
+        'password': password
+    })
+    
+    if serializer.is_valid():
+        tokens = serializer.validated_data
+        return Response({
+            'success': True,
+            'message': MessagesES.SUCCESS_LOGIN,
+            'access': tokens['access'],
+            'refresh': tokens['refresh']
+        })
+    else:
+        raise ValidationError(serializer.errors)
