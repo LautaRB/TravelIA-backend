f6ddbea8ac57c04c497e9cf5a67db0b37851c0a9
Lautaro Rodriguez Brown <lautanba@gmail.com>
2025-06-29 15:32:25 -0300

    Creación de ruta y medio

--- viaje/models.py
+++ viaje/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from .models import Ruta, Medio
+
+User = get_user_model()
 
 # Create your models here.
+class Viaje(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="viajes")
+    ruta = models.ForeignKey(Ruta, on_delete=models.SET_NULL, null=True, blank=True)
+    medio = models.ForeignKey(Medio, on_delete=models.SET_NULL, null=True, blank=True)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username}: {self.ruta} ({self.fecha_inicio})"
