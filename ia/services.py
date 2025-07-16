9ea93f81b691d1e93b0bc0504d443509f0373363
LautaRB <lautanba@gmail.com>
2025-07-16 16:21:09 -0300

    Bases para la implementación de la IA

---
+++ ia/services.py
@@ -0,0 +1,17 @@
+import openai
+from django.conf import settings
+
+openai.api_key = settings.OPENAI_API_KEY
+#En proceso...
+def generar_plan_viaje(datos, user):
+    prompt = f"""Hola! Quiero organizar un viaje del {datos['fecha_inicio']} al {datos['fecha_fin']}
+    partiendo desde {datos['origen']} hasta {datos['destino']}.
+    Por favor recomendame las 3 mejores rutas y los 3 mejores medios de transporte. Muchas gracias!"""
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[{"username": {user.username}, "role": "user", "content": prompt}]
+    )
+
+    contenido = response['choices'][0]['message']['content']
+    return {"plan": contenido}
--- ia/views.py
+++ ia/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .services import generar_plan_viaje  # lógica que vos vas a definir
 
 # Create your views here.
+class PlanearViajeIA(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        datos_usuario = request.data
+        respuesta_ia = generar_plan_viaje(datos_usuario, user=request.user)
+        return Response(respuesta_ia)
