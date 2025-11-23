2d6ba3c7466078b5d58b62b5220e830f9265ba48
Lautaro Rodriguez Brown <lautanba@gmail.com>
2025-11-22 22:48:23 -0300

    Modificación de origenes CORS

--- travelia/settings.py
+++ travelia/settings.py
@@ -136,6 +136,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8081",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:8081",
+    "http://localhost:4321",
+    "http://127.0.0.1:4321",
     "https://travelia-frontend.web.app",
     "https://travelia-frontend.firebaseapp.com",
 ]
